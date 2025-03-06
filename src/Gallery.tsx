31989236035900bb644c3823a83889c0f5ec5643
Yuliia Skrypnyk <jillyjoolli@gmail.com>
2025-03-07 00:37:43 +0100

    Add button

--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -5,10 +5,11 @@ import {CharacterList} from "./CharacterList.tsx";
 export const Gallery = () => {
     const [characters] = useState(response.results);
     const [search, setSearch] = useState("");
+    const [visibleCount, setVisibleCount] = useState(5);
 
     const filteredCharacters = characters.filter((char) =>
-        char.name.toLowerCase().includes(search.toLowerCase())
-    );
+        char.name.toLowerCase().includes(search.toLowerCase()))
+        .slice(0, visibleCount);
 
     return (
         <div>
@@ -20,6 +21,11 @@ export const Gallery = () => {
                 onChange={(e) => setSearch(e.target.value)}
             />
             <CharacterList characters={filteredCharacters} />
+            {visibleCount < characters.length && (
+                <button onClick={() => setVisibleCount((prev) => prev + 5)}>
+                    Show More
+                </button>
+            )}
         </div>
     );
 };
